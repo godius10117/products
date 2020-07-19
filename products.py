411126dcb58c4e8ac9647cc87d43d5044fae44dc
Jackie Chang <Jackie@Jackies-MacBook-Air.local>
2020-07-19 15:11:25 +0800

    add read file

--- products.py
+++ products.py
@@ -2,8 +2,21 @@
 #while loop 適用於不知道會執行幾次的情況
 #for i in range(100) > 有固定次數
 
-
+# 讀取檔案
+# split切割完的結果是清單
 products = []
+with open('products.csv', 'r', encoding ='UTF-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 跳過直接到下一回，用於迴圈(for loop/while loop)常用於不讀取檔案中的標題
+		name, price = line.strip().split(',')
+		#s = line.strip().split(',') 同上方一行完成
+		#name = s[0]
+		#price = s[1]
+		products.append([name,price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q': # ==等於，比較對象是否相等
@@ -16,14 +29,16 @@ while True:
 	#p.append(price)
 	#products.append(p)
 print(products)
-#存取二維清單方式 products[0][0](第一個清單內的第一個項目)
+# 存取二維清單方式 products[0][0](第一個清單內的第一個項目)
 
+# 印出所有購買紀錄
 for p in products:
 	# print(p) #印出商品及價格
 	# print(p[0]) #印出商品
 	print(p[0], '的價格是', p[1])
 
-with open('prodcuts.csv','w', encoding = 'UTF-8') as f:
+# 寫入檔案
+with open('products.csv','w', encoding= 'UTF-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] +',' + str(p[1]) + '\n')
