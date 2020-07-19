a491835d2dfffc38e9f6a325e85154875d0c9721
Jackie Chang <Jackie@Jackies-MacBook-Air.local>
2020-07-19 15:36:47 +0800

    add check file exist

--- products.py
+++ products.py
@@ -2,19 +2,24 @@
 #while loop 適用於不知道會執行幾次的情況
 #for i in range(100) > 有固定次數
 
+import os #operation system
+
 # 讀取檔案
-# split切割完的結果是清單
 products = []
-with open('products.csv', 'r', encoding ='UTF-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue # 跳過直接到下一回，用於迴圈(for loop/while loop)常用於不讀取檔案中的標題
-		name, price = line.strip().split(',')
-		#s = line.strip().split(',') 同上方一行完成
-		#name = s[0]
-		#price = s[1]
-		products.append([name,price])
-print(products)
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+	print('Yeah!找到檔案了!')
+	with open('products.csv', 'r', encoding ='UTF-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue # 跳過直接到下一回，用於迴圈(for loop/while loop)常用於不讀取檔案中的標題
+			name, price = line.strip().split(',') # split切割完的結果是清單
+			#s = line.strip().split(',') 同上方一行完成
+			#name = s[0]
+			#price = s[1]
+			products.append([name,price])
+	print(products)	
+else:
+	print('找不到檔案...')
 
 # 讓使用者輸入
 while True:
@@ -42,8 +47,6 @@ with open('products.csv','w', encoding= 'UTF-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] +',' + str(p[1]) + '\n')
-
-
 
 
 
