0d241ee2d16a51139fab670f7e97a6a3370616aa
Jackie Chang <Jackie@Jackies-MacBook-Air.local>
2020-07-22 08:49:21 +0800

    refactor

--- products.py
+++ products.py
@@ -1,52 +1,58 @@
-#while loop vs. for loop
-#while loop 適用於不知道會執行幾次的情況
-#for i in range(100) > 有固定次數
-
 import os #operation system
 
 # 讀取檔案
-products = []
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-	print('Yeah!找到檔案了!')
-	with open('products.csv', 'r', encoding ='UTF-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='UTF-8') as f:
 		for line in f:
 			if '商品,價格' in line:
-				continue # 跳過直接到下一回，用於迴圈(for loop/while loop)常用於不讀取檔案中的標題
-			name, price = line.strip().split(',') # split切割完的結果是清單
-			#s = line.strip().split(',') 同上方一行完成
-			#name = s[0]
-			#price = s[1]
+				continue 
+			name, price = line.strip().split(',')
 			products.append([name,price])
 	print(products)	
-else:
-	print('找不到檔案...')
+	return products
 
 # 讓使用者輸入
-while True:
-	name = input('請輸入商品名稱：')
-	if name == 'q': # ==等於，比較對象是否相等
-		break
-	price = input('請輸入商品價格：')
-	price = int(price)
-	products.append([name, price]) #簡化寫法，效果同下三行
-	#p = [] # 創造二維清單(2dimensional, xy軸)
-	#p.append(name)
-	#p.append(price)
-	#products.append(p)
-print(products)
-# 存取二維清單方式 products[0][0](第一個清單內的第一個項目)
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱：')
+		if name == 'q': 
+			break
+		price = input('請輸入商品價格：')
+		price = int(price)
+		products.append([name, price]) 
+	print(products)
+	return products
 
 # 印出所有購買紀錄
-for p in products:
-	# print(p) #印出商品及價格
-	# print(p[0]) #印出商品
-	print(p[0], '的價格是', p[1])
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv','w', encoding= 'UTF-8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] +',' + str(p[1]) + '\n')
+def write_file(filename, products):
+	with open(filename,'w', encoding='UTF-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] +',' + str(p[1]) + '\n')
+	return products
+
+#main()function 裝主要程式碼，作為程式的進入點
+def main(): 
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('Yeah!找到檔案了!')
+		products = read_file(filename)
+	else:
+		print('找不到檔案')
+	products = user_input(products)
+	print_products(products)
+	products = write_file('products.csv', products)
+
+main()
+
+# function中心思想只做一件事
+# refactor(重構程式)就是把程式碼不斷改寫，寫成越來越小的function
 
 
 
